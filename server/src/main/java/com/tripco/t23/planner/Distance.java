f17f5f4a1972db82aa293ab7379cd3348d045837
swestra <swestra13@gmail.com>
2018-09-05 23:18:57 -0600

    Here's the general setup

--- server/src/main/java/com/tripco/t23/planner/Distance.java
+++ server/src/main/java/com/tripco/t23/planner/Distance.java
@@ -17,6 +17,20 @@ public class Distance {
     public int distance;
 
     //Function for the actual calculation
-    public static void calculate(){
+    public void calculate(){
+        //Angles converted to radians
+        double lat1 = Math.toRadians(Double.parseDouble(origin.latitude));
+        double log1 = Math.toRadians(Double.parseDouble(origin.longitude));
+        double lat2 = Math.toRadians(Double.parseDouble(destination.latitude));
+        double log2 = Math.toRadians(Double.parseDouble(destination.longitude));
+        //double difLat = Math.abs(lat1 - lat2);
+        double difLog = Math.abs(log1 - log2);
+
+        //Equation attempt
+        double numerator1 = Math.pow(Math.cos(lat2) * Math.sin(difLog), 2);
+        double numerator2 = Math.pow(Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(difLog),2);
+        double denominator = Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(difLog);
+        double temp = Math.atan2(Math.sqrt(numerator1 + numerator2),denominator);
+        distance = (int) Math.round(temp * 3959);
     }
 }
