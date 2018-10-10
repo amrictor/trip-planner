e3aed9a087cd596e22163a15d709ef643f5dbf61
jhgrins <jhgrinslade@hotmail.com>
2018-10-09 20:09:39 -0600

    Created part of file upload (just extracted contents and put into json)

--- client/src/components/Application/Plan.js
+++ client/src/components/Application/Plan.js
@@ -5,19 +5,30 @@ import { ButtonGroup, Button } from 'reactstrap'
 class Plan extends Component {
     constructor(props) {
         super(props);
+        this.getFile = this.getFile.bind(this);
     }
+
+    getFile(event){
+        let reader = new FileReader();
+        reader.onload = function(event) {
+            let fileContent = event.target.result;
+            let contents = JSON.parse(fileContent);
+            console.log(contents);
+            this.props.updateBasedOnResponse(contents);
+        }.bind(this);
+        reader.readAsText(event.target.files[0]);
+    }
+
+
     render() {
         return (
             <Card>
-
                 <CardBody>
                     <CardTitle>Plan</CardTitle>
-                    <p><b>Upload your trip file:</b></p>
-                    <ButtonGroup>
-                        {<form enctype="multipart/form-data" action="Resources/userupload" method="post">
-                            <input id="image-file" type="file" />
-                        </form>}
-                    </ButtonGroup>
+                    <p><b>Upload your trip file: </b></p>
+                    <form>
+                        <input type="file" name="myFile" id="example" onChange={(event) =>this.getFile(event)}/>
+                    </form>
                 </CardBody>
             </Card>
         )
