9245a1b742e7b5130cef194750cc85e295a2a808
khanhnguyenle <31377219+khanhnguyenle@users.noreply.github.com>
2018-10-10 14:53:09 -0600

    user define unit completed #71

--- client/src/components/Application/Options.js
+++ client/src/components/Application/Options.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import { Card, CardHeader, CardTitle, CardBody } from 'reactstrap'
 import { ButtonGroup, Button } from 'reactstrap'
+import { InputGroup, Input } from 'reactstrap'
 
 /* Options allows the user to change the parameters for planning
  * and rendering the trip map and itinerary.
@@ -10,20 +11,49 @@ import { ButtonGroup, Button } from 'reactstrap'
 class Options extends Component{
   constructor(props) {
     super(props);
+    this.state = {isUserDef: false};
+  }
+
+  checkButton(event){
+      const unit = event.target.value;
+      if(unit == 'user defined'){
+          this.setState({isUserDef: true});
+          this.props.updateOptions('units', event.target.value)
+      }
+      else {
+          this.setState({isUserDef: false});
+          this.props.updateOptions('units', event.target.value)
+      }
+  }
+
+  userDefValues(name,radius){
+      this.props.updateOptions('unitName',name)
+      this.props.updateOptions('unitRadius',radius)
   }
 
   render() {
+    const isUserDef = this.state.isUserDef;
     const buttons = this.props.config.units.map((units) =>
       <Button
         key={'distance_button_' + units}
         className='btn-outline-dark unit-button'
         active={this.props.options.units === units}
         value={units}
-        onClick={(event) => this.props.updateOptions('units', event.target.value)}
+        onClick={(event) => this.checkButton(event)}
+
       >
         {units.charAt(0).toUpperCase() + units.slice(1)}
       </Button>
     );
+    let userdeffield;
+    if(isUserDef){
+        userdeffield =
+            <InputGroup id="userdef" isUserDef={isUserDef}>
+                <Input type="text" name="name" placeholder="Unit name"/>
+                <Input type="float" name="radius" placeholder="Earth radius"/>
+                <input type="button" onclick="userDefValues(name,radius)" value="Submit"/>
+            </InputGroup>
+    }
 
     return(
       <Card>
@@ -34,6 +64,7 @@ class Options extends Component{
           <ButtonGroup>
             {buttons}
           </ButtonGroup>
+            <div>{userdeffield}</div>
         </CardBody>
         <CardBody>
             <p><b>Enter your server port:</b></p>
