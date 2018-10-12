7d20a761c3aec83c4f322805953581ce8ac3403e
swestra <swestra13@gmail.com>
2018-10-11 23:05:32 -0600

    Version number changed to 2.7

--- server/src/main/java/com/tripco/t23/server/Config.java
+++ server/src/main/java/com/tripco/t23/server/Config.java
@@ -3,14 +3,17 @@ package com.tripco.t23.server;
 import com.google.gson.Gson;
 import com.tripco.t23.planner.Optimization;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 public class Config {
 
   private short version = 3;
   private String type = "config";
   private List<String> units = Arrays.asList("miles", "nautical miles", "kilometers", "user defined");
-  private ArrayList<Optimization> optimization = new ArrayList<>(Arrays.asList(new Optimization("none","The trip is not optimized"),
+  private ArrayList<Optimization> optimization = new ArrayList<>(
+          Arrays.asList(new Optimization("none","The trip is not optimized"),
           new Optimization("short","Nearest neighbor"),
           new Optimization("shorter","2-opt")));
   private Integer port;
