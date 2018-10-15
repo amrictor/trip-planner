da7d3722bc6a45cad0221bf5681b972750fff5ec
khanhnguyenle <31377219+khanhnguyenle@users.noreply.github.com>
2018-10-15 14:22:04 -0600

    added unitName and unitRadius to constructor state
    Looks like a lot but it's just the code reformatting itself to correct indentation.

--- client/src/components/Application/Application.js
+++ client/src/components/Application/Application.js
@@ -12,89 +12,91 @@ import { get_config } from '../../api/api';
  * Holds the destinations and options state shared with the trip.
  */
 class Application extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      config: null,
-      trip: {
-        type: "trip",
-        title: "",
-        options : {
-          units: "miles"
-        },
-        places: [],
-        distances: [],
-        map: '<svg width="1920" height="20" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg"><g></g></svg>'
-      },
-      port: window.location.port,
-      host: window.location.host
-    };
-    this.updateTrip = this.updateTrip.bind(this);
-    this.updateBasedOnResponse = this.updateBasedOnResponse.bind(this);
-    this.updateOptions = this.updateOptions.bind(this);
-    this.updateHostAndPort = this.updateHostAndPort.bind(this);
+    constructor(props){
+        super(props);
+        this.state = {
+            config: null,
+            trip: {
+                type: "trip",
+                title: "",
+                options : {
+                    units: "miles",
+                    unitName: "",
+                    unitRadius: 0
+                },
+                places: [],
+                distances: [],
+                map: '<svg width="1920" height="20" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg"><g></g></svg>'
+            },
+            port: window.location.port,
+            host: window.location.host
+        };
+        this.updateTrip = this.updateTrip.bind(this);
+        this.updateBasedOnResponse = this.updateBasedOnResponse.bind(this);
+        this.updateOptions = this.updateOptions.bind(this);
+        this.updateHostAndPort = this.updateHostAndPort.bind(this);
 
-  }
+    }
 
-  componentWillMount() {
-    get_config().then(
-      config => {
-        this.setState({
-          config:config
-        })
-      }
-    );
-  }
+    componentWillMount() {
+        get_config().then(
+            config => {
+                this.setState({
+                    config:config
+                })
+            }
+        );
+    }
 
-  updateHostAndPort(value){
-      this.port = value;
-      this.host = "black-bottle.cs.colostate.edu";
-  }
+    updateHostAndPort(value){
+        this.port = value;
+        this.host = "black-bottle.cs.colostate.edu";
+    }
 
-  updateTrip(field, value){
-    let trip = this.state.trip;
-    trip[field] = value;
-    this.setState(trip);
-  }
+    updateTrip(field, value){
+        let trip = this.state.trip;
+        trip[field] = value;
+        this.setState(trip);
+    }
 
-  updateBasedOnResponse(value) {
-    this.setState({'trip': value});
-  }
+    updateBasedOnResponse(value) {
+        this.setState({'trip': value});
+    }
 
-  updateOptions(option, value){
-    let trip = this.state.trip;
-    trip.options[option] = value;
-    this.setState(trip);
-  }
+    updateOptions(option, value){
+        let trip = this.state.trip;
+        trip.options[option] = value;
+        this.setState(trip);
+    }
 
-  render() {
-    if(!this.state.config) { return <div/> }
+    render() {
+        if(!this.state.config) { return <div/> }
 
-    return(
-      <Container id="Application">
-        <Info/>
-        <Plan
-            updateBasedOnResponse={this.updateBasedOnResponse}
-            trip={this.state.trip} 
-            port={this.port}
-            host={this.host}
-        />
-        <Itinerary
-            trip={this.state.trip}
-        />
-        <Map
-            svg={this.state.trip.map}
-        />
-        <Calculator/>
-        <Options
-            options={this.state.trip.options}
-            config={this.state.config}
-            updateOptions={this.updateOptions}
-            updateHostAndPort={this.updateHostAndPort}
-        />
-      </Container>
-    )
-  }
+        return(
+            <Container id="Application">
+              <Info/>
+              <Plan
+                  updateBasedOnResponse={this.updateBasedOnResponse}
+                  trip={this.state.trip}
+                  port={this.port}
+                  host={this.host}
+              />
+              <Itinerary
+                  trip={this.state.trip}
+              />
+              <Map
+                  svg={this.state.trip.map}
+              />
+              <Calculator/>
+              <Options
+                  options={this.state.trip.options}
+                  config={this.state.config}
+                  updateOptions={this.updateOptions}
+                  updateHostAndPort={this.updateHostAndPort}
+              />
+            </Container>
+        )
+    }
 }
 
 export default Application;
