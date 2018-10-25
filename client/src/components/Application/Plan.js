459a6900766a7cfabc4874ceae815e3d2cdc152b
Abigail Rictor <amrictor@dorado.cs.colostate.edu>
2018-10-24 23:51:35 -0600

    major refactoring! created Search.js from code in plan, recreated Map.js from code in Itinerary.js, also big UI update

--- client/src/components/Application/Plan.js
+++ client/src/components/Application/Plan.js
@@ -5,8 +5,11 @@ import { Col, Container, Row, Table } from 'reactstrap';
 import { ButtonGroup, Button } from 'reactstrap';
 import { request, get_config } from '../../api/api';
 import Itinerary from './Itinerary';
+import Map from './Map';
+import Search from './Search';
 import {Collapse} from 'reactstrap';
 import {Form} from 'reactstrap';
+
 import {Input, InputGroup, InputGroupAddon} from 'reactstrap'
 import {Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
@@ -16,28 +19,10 @@ class Plan extends Component {
         this.getFile = this.getFile.bind(this);
         this.planRequest = this.planRequest.bind(this);
         this.state = {
-            search: {
-                version: 3,
-                type: "search",
-                match: "",
-                limit: 10,
-                places: []
-            },
-            dropdownOpen: false,
-            showComponent: false,
-            isload: false,
-            isSearch: false,
-            isadd: false
+            isLoad: false,
+            isSearch: false
         };
         this.updateBasedOnResponse = this.updateBasedOnResponse.bind(this);
-        this.toggleSearch = this.toggleSearch.bind(this);
-        this.showSearchResult = this.showSearchResult.bind(this);
-        this.toggleDropdown = this.toggleDropdown.bind(this);
-    }
-    updateSearch() {
-        let search = this.state.search;
-        search['match'] = query_field.value;
-        this.setState(search)
     }
     updateBasedOnResponse(value) {
         this.setState({'search': value});
@@ -67,13 +52,12 @@ class Plan extends Component {
             this.updateBasedOnResponse(response)
         });
         this.toggleSearch();
-        //request(this.props.search, 'search', this.props.port, this.props.host).then(response => this.props.updateSearchBasedOnResponse(response));
-        //NOW SEARCH STATE IS UPDATED. JUST SHOW THE RESULT, ADD BUTTON IS ALREADY IMPLEMENTED, WE ARE SETTING LIMIT TO 10 FOR NOW
     }
 
     addPlace(id, name, lat, long){
         const place = {'id': id, 'name': name, 'latitude': lat, 'longitude': long};
         this.props.updatePlaces(place);
+        this.planRequest();
     }
 
     clearFileUploader(){
@@ -106,190 +90,141 @@ class Plan extends Component {
             document.body.removeChild(a);
         }
     }
-    toggleDropdown() {
-        this.setState({ dropdownOpen: !this.state.dropdownOpen });
-    }
     toggleLoad() {
-        this.setState({ isload: !this.state.isload });
-    }
-    toggleSearch() {
-        this.setState({ isSearch: !this.state.isSearch });
-    }
-    toggleAdd() {
-        this.setState({ isadd: !this.state.isadd });
-    }
-    putData(){
-        let data = [];
-        for (let i = 0; i < this.state.search.places.length; i++){
-            data.push(<tr key={this.state.search.places[i].name}>
-                <td>{this.state.search.places[i].id}</td>
-                <td>{this.state.search.places[i].name}</td>
-                <td>{Math.round((this.state.search.places[i].latitude+ 0.00001) * 100)/100}</td>
-                <td>{Math.round((this.state.search.places[i].longitude+ 0.00001) * 100)/100}</td>
-                <td>
-                    <Button
-                        key={'options_submit'}
-                        className='btn-outline-dark unit-button'
-                        onClick={()=> this.addPlace(this.state.search.places[i].id, this.state.search.places[i].name, this.state.search.places[i].latitude, this.state.search.places[i].longitude)}
-                    >
-                        Add
-                    </Button>
-                </td>
-            </tr>);
-            if(i==this.state.search.limit) break;
-        }
-        return data;
-    }
-    searchPopUp() {
-        if (typeof this.state.search !== "undefined" && this.state.search.places.length > 0) {
-
-            return (
-                <Table>
-                    <thead>
-                        <tr>
-                            <th scope="row">ID</th>
-                            <th>Name</th>
-                            <th>Lat</th>
-                            <th>Long</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {this.putData()}
-                    </tbody>
-                </Table>
-        );
-
-        }
-        return "No results available."
+        this.setState({ isLoad: !this.state.isLoad });
     }
 
     render() {
         const fileuploader =
-            <Collapse isOpen={this.state.isload}>
-                <p><b>Upload your trip file: </b></p>
+            <Collapse isOpen={this.state.isLoad}>
+                <p>Upload your trip file:</p>
                 <form>
                     <input type="file" name="myFile" id="example" onChange={(event) => {this.getFile(event)}}/>
                 </form>
             </Collapse>;
 
-        const searchquery=
-                <Form inline>
-                    <InputGroup>
-                        <Input
-                            type="text"
-                            name="query"
-                            id="query_field"
-                            placeholder="Query"
-                            onChange={()=>this.updateSearch()}
-                        />&nbsp;
-                        <InputGroupAddon addonType="append">
-                        <Button
-                            key={'options_submit'}
-                            className='btn-outline-dark unit-button'
-                            onClick={()=> this.showSearchResult()}
-                        >
-                            Search
-                        </Button>
-                        </InputGroupAddon>
-                    </InputGroup>
-                </Form>;
+        const functions =
+            <ButtonGroup>
+                <Button
+                    key={'load'}
+                    color= "primary" style={{ marginBottom: '1rem' }}
+                    className='btn-outline-dark unit-button'
+                    onClick={()=> this.toggleLoad()}
+                    active={this.state.isLoad}
+                >
+                    Load
+                </Button>
+                <Button
+                    key={'clear'}
+                    color= "primary" style={{ marginBottom: '1rem' }}
+                    className='btn-outline-dark unit-button'
+                    onClick={()=> this.clearFileUploader()}
+                >
+                    Clear
+                </Button>
+                <Button
+                    key={'plan'}
+                    color= "primary" style={{ marginBottom: '1rem' }}
+                    className='btn-outline-dark unit-button'
+                    onClick={() => this.planRequest()}
+                >
+                    Plan
+                </Button>
+                <Button
+                    key={'save'}
+                    color= "primary" style={{ marginBottom: '1rem' }}
+                    className='btn-outline-dark unit-button'
+                    onClick={() => this.saveToFile()}
+                >
+                    Save
+                </Button>
+
+            </ButtonGroup>
+            ;
 
         const addBody =
 
-            <Form inline>
-                <InputGroup>
-                <Input
-                    type="text"
-                    name="id"
-                    id="id_field"
-                    placeholder="ID"
-                />
-                <Input
-                    type="text"
-                    name="name"
-                    id="name_field"
-                    placeholder="Name"
-                />
-                <Input
-                    type="number"
-                    name="latitude"
-                    id="latitude_field"
-                    placeholder="Latitude"
-                />
-                <Input
-                    type="number"
-                    name="longitude"
-                    id="longitude_field"
-                    placeholder="Longitude"
-                /> &nbsp;
-                <InputGroupAddon addonType="append"><Button
+            <React.Fragment>
+                <Form>
+                    <InputGroup>
+                        <Input
+                            type="text"
+                            name="id"
+                            id="id_field"
+                            placeholder="ID"
+                        />
+                        <Input
+                            type="text"
+                            name="name"
+                            id="name_field"
+                            placeholder="Name"
+                        />
+                    </InputGroup>
+                </Form>
+                <Form>
+                    <InputGroup>
+                        <Input
+                            type="number"
+                            name="latitude"
+                            id="latitude_field"
+                            placeholder="Latitude"
+                        />
+                        <Input
+                            type="number"
+                            name="longitude"
+                            id="longitude_field"
+                            placeholder="Longitude"
+                        />
+                        {/*<InputGroupAddon addonType="append"></InputGroupAddon>*/}
+                    </InputGroup>
+                </Form>
+                <Button
                     key={'options_submit'}
                     className='btn-outline-dark unit-button'
                     onClick={()=> this.addPlace(id_field.value, name_field.value, latitude_field.value, longitude_field.value)}
+                    block
                 >
-                    Add
-                </Button></InputGroupAddon>
-                </InputGroup>
-            </Form>;
+                    &#x2795;
+                </Button>
+            </React.Fragment>;
+
 
         return (
             <React.Fragment>
                 <Card>
                     <CardBody id="Plan">
                         <CardTitle>Plan a trip around Colorado!</CardTitle>
-                        <ButtonGroup>
-                            <Button
-                                key={'load'}
-                                color= "primary" style={{ marginBottom: '1rem' }}
-                                className='btn-outline-dark unit-button'
-                                onClick={()=> this.toggleLoad()}
-                                active={this.state.isload === true}
-                            >
-                                Load
-                            </Button>
-                            <Button
-                                key={'clear'}
-                                color= "primary" style={{ marginBottom: '1rem' }}
-                                className='btn-outline-dark unit-button'
-                                onClick={()=> this.clearFileUploader()}
-                                disabled={this.state.isloaded === false}
-                            >
-                                Clear
-                            </Button>
-                            <Button
-                                key={'plan'}
-                                color= "primary" style={{ marginBottom: '1rem' }}
-                                className='btn-outline-dark unit-button'
-                                onClick={() => this.planRequest()}
-                                disabled={this.state.isloaded === false}
-                            >
-                                Plan
-                            </Button>
-                            <Button
-                                key={'save'}
-                                color= "primary" style={{ marginBottom: '1rem' }}
-                                className='btn-outline-dark unit-button'
-                                onClick={() => this.saveToFile()}
-                                disabled={this.state.isloaded === false}
-                            >
-                                Save
-                            </Button>
-                        </ButtonGroup>
-                        {fileuploader}
-                    </CardBody>
-                    <CardBody id="add">
-                        <CardTitle>Add stops to your trip and click plan to update your itinerary.</CardTitle>
-                        {searchquery}
-                        <Collapse isOpen={this.state.isSearch}>
-                            {this.searchPopUp()}
-                        </Collapse>
-                        {addBody}
+                        <hr/>
+                        <Container>
+                            <Row>
+                                <Col  xs="5">
+                                    {functions}
+                                    {fileuploader}
+                                    <CardTitle>Add stops to your trip!</CardTitle>
+                                    {addBody}
+                                    <hr/>
+                                </Col>
+
+                                <Col>
+                                    <Map
+                                        trip={this.props.trip}
+                                    />
+                                </Col>
+                            </Row>
+                            <hr/>
+                            <Search
+                                updatePlaces={this.props.updatePlaces}
+                                planRequest={this.planRequest}
+                                addPlace={this.addPlace}
+                                places={this.state.places}
+                                port={this.port}
+                                host={this.host}/>
+                            <Itinerary
+                                trip={this.props.trip}
+                            />
+                        </Container>
                     </CardBody>
                 </Card>
-                <Itinerary
-                    trip={this.props.trip}
-                />
             </React.Fragment>
         )
     }
---
+++ client/src/components/Application/Search.js
@@ -0,0 +1,148 @@
+import React, {Component} from 'react';
+import { Modal, ModalHeader, ModalFooter, ModalBody } from 'reactstrap';
+import { Card, CardBody, CardTitle, CardSubtitle, CardImg } from 'reactstrap';
+import { Col, Container, Row, Table } from 'reactstrap';
+import { ButtonGroup, Button } from 'reactstrap';
+import {Collapse} from 'reactstrap';
+import {Form} from 'reactstrap';
+import {Input, InputGroup, InputGroupAddon} from 'reactstrap'
+import { request, get_config } from '../../api/api';
+
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            search: {
+                version: 3,
+                type: "search",
+                match: "",
+                limit: 5,
+                places: []
+            },
+            isSearch: false,
+        };
+        this.updateBasedOnResponse = this.updateBasedOnResponse.bind(this);
+        this.showSearchResult = this.showSearchResult.bind(this);
+    }
+    updateSearch() {
+        let search = this.state.search;
+        search['match'] = query_field.value;
+        this.setState(search)
+    }
+    updateBasedOnResponse(value) {
+        this.setState({'search': value});
+    }
+    showSearchResult(){
+        request(this.state.search, 'search', this.props.port, this.props.host).then(response => {
+            this.updateBasedOnResponse(response)
+        });
+        this.setState({ isSearch: true });
+    }
+    closeSearch() {
+        query_field.value ="";
+        this.updateSearch();
+        this.setState({ isSearch: false });
+    }
+    addPlace(id, name, lat, long){
+        const place = {'id': id, 'name': name, 'latitude': lat, 'longitude': long};
+        this.props.updatePlaces(place);
+        this.props.planRequest();
+    }
+    putData(){
+        let data = [];
+        for (let i = 0; i < this.state.search.places.length; i++){
+            data.push(
+                <React.Fragment key={this.state.search.places[i].id}>
+                <Row>
+                    <Col xs="2" key='id'>{this.state.search.places[i].id}</Col>
+                    <Col xs="4" key='name'>{this.state.search.places[i].name}</Col>
+                    <Col xs="2" key='lat'>{String(Math.round((this.state.search.places[i].latitude+ 0.00001) * 100)/100)}</Col>
+                    <Col xs="2" key='lon'>{String(Math.round((this.state.search.places[i].longitude+ 0.00001) * 100)/100)}</Col>
+                    <Col xs="1">
+                        <Button
+                            key={'options_submit'}
+                            className='btn-outline-dark unit-button'
+                            onClick={()=> this.addPlace(this.state.search.places[i].id, this.state.search.places[i].name, this.state.search.places[i].latitude, this.state.search.places[i].longitude)}
+                        >
+                            &#x2795;
+                        </Button>
+                    </Col>
+                </Row>
+                <hr/>
+                </React.Fragment>
+            );
+            if(i==this.state.search.limit) break;
+        }
+        return data;
+    }
+    searchResults() {
+        if (typeof this.state.search !== "undefined" && this.state.search.places.length > 0) {
+
+            return (
+                <React.Fragment>
+                    <Container>
+                        <Row>
+                            <Col xs="2" key='id'>ID</Col>
+                            <Col xs="4" key='name'>Name</Col>
+                            <Col xs="2" key='lat'>Lat</Col>
+                            <Col xs="2" key='lon'>Lon</Col>
+                        </Row>
+                        <hr/>
+                    {this.putData()}
+                    </Container>
+                </React.Fragment>
+        );
+
+        }
+        return "No results available."
+    }
+
+    render() {
+        const searchquery=
+        <React.Fragment>
+
+            <Form>
+                <InputGroup>
+                    <Input
+                        type="text"
+                        name="query"
+                        id="query_field"
+                        placeholder="Search"
+                        onChange={()=>this.updateSearch()}
+                    />
+                    <InputGroupAddon addonType="append">
+                        <Button
+                            key={'options_submit'}
+                            className='btn-outline-dark unit-button'
+                            onClick={()=> this.showSearchResult()}
+                        >
+                            &#x1f50d;
+                        </Button>
+                    </InputGroupAddon>
+                    <InputGroupAddon addonType="append">
+                        <Button
+                            key={'options_submit'}
+                            className='btn-outline-dark unit-button'
+                            onClick={()=> this.closeSearch()}
+                        >
+                            &#x274c;
+                        </Button>
+                    </InputGroupAddon>
+                </InputGroup>
+            </Form>
+
+        </React.Fragment>;
+
+        return (
+            <React.Fragment>
+                <CardTitle>Don't know your stop?</CardTitle>
+                {searchquery}
+                <Collapse isOpen={this.state.isSearch}>
+                    {this.searchResults()}
+                </Collapse>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Search;
