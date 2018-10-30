2789f91431b87f1577ac6b6983483939b529a246
swestra <swestra13@gmail.com>
2018-10-30 15:05:02 -0600

    Cleaning up

--- server/src/main/java/com/tripco/t23/planner/TripOpt.java
+++ server/src/main/java/com/tripco/t23/planner/TripOpt.java
@@ -21,8 +21,8 @@ public class TripOpt {
         this.places = places;
         this.units = units;
         currentShortest = Integer.MAX_VALUE;
-        for(int i = 0; i < places.size();i++){
-            for(int j = 0; j < places.size();i++){
+        for(int i = 0; i < places.size()-1;i++){
+            for(int j = 0; j < places.size()-1;i++){
                 allDistances[i][j] = measure(places.get(i),places.get(j));
             }
         }
