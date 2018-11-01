c72206a6cdbd62c75df4bd0816bc328b306fe030
jhgrins <jhgrinslade@hotmail.com>
2018-11-01 13:18:23 -0600

    Added a Filter Object and updated Config TFFI

--- server/src/main/java/com/tripco/t23/server/Config.java
+++ server/src/main/java/com/tripco/t23/server/Config.java
@@ -2,6 +2,7 @@ package com.tripco.t23.server;
 
 import com.google.gson.Gson;
 import com.tripco.t23.planner.Optimization;
+import com.tripco.t23.planner.Filter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,7 +12,7 @@ import java.util.List;
 public class Config {
 
   private String type = "config";
-  private short version = 3;
+  private short version = 4;
   private List<String> units = Arrays.asList("miles", "nautical miles", "kilometers", "user defined");
   private String unitName;
   private Double unitRadius;
@@ -20,6 +21,9 @@ public class Config {
           new Optimization("short","Nearest neighbor"),
           new Optimization("shorter","2-opt")));
   private List<String> attributes = Arrays.asList("name","id","latitude","longitude");
+  private ArrayList<Filter> filters = new ArrayList<>(
+          Arrays.asList(new Filter("type", new ArrayList<>(Arrays.asList("balloonport", "heliport", "airport", "seaplane base"))))
+          );
   private Integer port;
 
 
