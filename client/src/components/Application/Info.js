5758dba6484a9de6882c7b842102a41d2dedc283
Khanh Nguyen Le <kle@rams.colostate.edu>
2018-11-30 09:04:18 -0700

    implement about us

--- client/src/components/Application/Info.js
+++ client/src/components/Application/Info.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {Card, CardHeader, CardBody, CardTitle, Container} from 'reactstrap'
+import teamPhoto from '/public/teamphoto.png'
 
 export default class Info extends Component {
     render() {
@@ -25,16 +26,8 @@ export default class Info extends Component {
                     <CardTitle>Meet the devs</CardTitle>
                     <hr/>
                     <ol >
-                        <li>
-                            Choose options for trip planning, information to display about locations,
-                            and how the trip map and itinerary should be saved.</li>
-                        <li>
-                            Choose your destinations by loading existing sets of destinations or
-                            find more in an extensive database of locations worldwide.</li>
-                        <li>
-                            Plan the trip with the options you selected.
-                            Review and revise the trip origin and order.
-                            Save the trip map and itinerary for future reference.</li>
+                        <img src={teamPhoto}/>
+                        Application developers: Khanh Nguyen Le, Josette Grinslade, Abigail Rictor, Sam Westra
                     </ol>
                 </CardBody>
             </Container>
