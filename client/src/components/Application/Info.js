8dfde0bb451f67cd2cc3c439c3cd18cbfdb939d4
Khanh Nguyen Le <kle@rams.colostate.edu>
2018-11-30 08:51:44 -0700

    implement about us

--- client/src/components/Application/Info.js
+++ client/src/components/Application/Info.js
@@ -21,6 +21,22 @@ export default class Info extends Component {
                     Save the trip map and itinerary for future reference.</li>
                 </ol>
               </CardBody>
+                <CardBody>
+                    <CardTitle>Meet the devs</CardTitle>
+                    <hr/>
+                    <ol >
+                        <li>
+                            Choose options for trip planning, information to display about locations,
+                            and how the trip map and itinerary should be saved.</li>
+                        <li>
+                            Choose your destinations by loading existing sets of destinations or
+                            find more in an extensive database of locations worldwide.</li>
+                        <li>
+                            Plan the trip with the options you selected.
+                            Review and revise the trip origin and order.
+                            Save the trip map and itinerary for future reference.</li>
+                    </ol>
+                </CardBody>
             </Container>
         )
     }
