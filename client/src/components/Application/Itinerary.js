befb87d8e4356621c93550b4b6add3ebd0af3945
amrictor <amrictor@gmail.com>
2018-12-03 18:08:31 -0700

    fix mobile layout

--- client/src/components/Application/Itinerary.js
+++ client/src/components/Application/Itinerary.js
@@ -15,6 +15,7 @@ class Itinerary extends Component {
         this.state = {
             itin: true,
             attributes: JSON.parse(JSON.stringify(this.props.config.attributes)),
+            total: 0
         };
         this.putData = this.putData.bind(this);
         this.createTable = this.createTable.bind(this);
@@ -35,22 +36,22 @@ class Itinerary extends Component {
     }
 
     putData() {
+        this.state.total=0;
         let data = [];
         if (typeof this.props.trip.places !== "undefined") {
             let size = this.props.trip.places.length;
-            let total = 0;
             if (typeof this.props.trip.distances !== "undefined") {
                 for (let i = 0; i < size; i++) {
                     let rowData = [];
                     for (let j = 0; j < this.state.attributes.length; j++) {
                         let attribute = this.state.attributes[j];
                         let size = !this.state.attributes.includes('name')
-                            ? Math.floor(10/(this.state.attributes.length+2))
+                            ? Math.floor(12/(this.state.attributes.length))
                             : (attribute==='name')
                                 ? 4
-                                : Math.floor(6/(this.state.attributes.length+1));
+                                : Math.floor(6/(this.state.attributes.length-1));
                         rowData.push(
-                            <Col xs={size} key={this.state.attributes[i]+'_'+i+'_'+j}>
+                            <Col md={size} key={this.state.attributes[i]+'_'+i+'_'+j}>
                                 {
                                     (typeof this.props.trip.places[i][attribute] === 'number')
                                     ? String(Math.round((this.props.trip.places[i][attribute]+ 0.00001) * 100)/100)
@@ -59,53 +60,50 @@ class Itinerary extends Component {
                             </Col>
                         );
                     }
-                    let size = !this.state.attributes.includes('name')
-                        ? Math.floor(10/(this.state.attributes.length+2))
-                        : Math.floor(6/(this.state.attributes.length+1));
-                    rowData.push(
-                        <React.Fragment key={'distances_'+i}>
-                            <Col xs={size} key={'leg_'+i}>
-                                {this.props.trip.distances[i]}
-                            </Col>
-                            <Col xs={size} key={'cumulative_'+i}>
-                                {total}
-                            </Col>
-                        </React.Fragment>
-                    );
                     data.push(
                         <React.Fragment key={this.props.trip.places[i].id}>
                             <Row>
-                                {rowData}
-                                <Col>
-                                    <Button
-                                        key={'add_submit_'+i}
-                                        className='btn-outline-dark unit-button float-right'
-                                        onClick={() => this.removePlace(this.props.trip.places[i].id, this.props.trip.places[i].name, this.props.trip.places[i].latitude, this.props.trip.places[i].longitude)}
-                                    >
-                                        &#x2796;
-                                    </Button>
-                                    <Button
-                                        key={'make_first_'+i}
-                                        className='btn-outline-dark unit-button float-right'
-                                        onClick={() => this.props.updatePlaces(i, "origin")}
-                                    >
-                                        &#x21a5;
-                                    </Button>
+                                <Col xs={'10'}>
+                                    <Row>
+                                        {rowData}
+                                    </Row>
                                 </Col>
+                                <Col xs={'2'}>
+                                    <ButtonGroup className='float-right'>
+                                        <Button
+                                            key={'add_submit_'+i}
+                                            className='btn-outline-dark unit-button '
+                                            onClick={() => this.removePlace(this.props.trip.places[i].id, this.props.trip.places[i].name, this.props.trip.places[i].latitude, this.props.trip.places[i].longitude)}
+                                        >
+                                            &#x2796;
+                                        </Button>
+                                        <Button
+                                            key={'make_first_'+i}
+                                            className='btn-outline-dark unit-button float-right'
+                                            onClick={() => this.props.updatePlaces(i, "origin")}
+                                        >
+                                            &#x21a5;
+                                        </Button>
+                                    </ButtonGroup>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <React.Fragment key={'distances_'+i}>
+                                    <Col sm="12" md={{size: 6, offset: 3}}>
+                                        <p>Leg Distance: <font color="#ff4500">{this.props.trip.distances[i]}</font>&nbsp;&nbsp;&nbsp;&nbsp;
+                                        Cumulative Distance: <font color="#ff4500">{this.state.total}</font></p>
+                                    </Col>
+                                </React.Fragment>
                             </Row>
                             <hr/>
                         </React.Fragment>
                     );
-                    total += this.props.trip.distances[i];
+                    this.state.total += this.props.trip.distances[i];
 
 
                 }
                 data.push(
-                    <Row key="Total">
-                        <Col sm="12" md={{size: 2, offset: 5}}>
-                            <b>Total: <font color="green">{total}</font></b>
-                        </Col>
-                    </Row>
+
                 );
             }
             return data;
@@ -117,29 +115,16 @@ class Itinerary extends Component {
         for (let i = 0; i < this.state.attributes.length; i++) {
             let attribute = this.state.attributes[i];
             let size = !this.state.attributes.includes('name')
-                ? Math.floor(10/(this.state.attributes.length+2))
+                ? Math.floor(10/(this.state.attributes.length))
                 : (attribute==='name')
                     ? 4
-                    : Math.floor(6/(this.state.attributes.length+1));
+                    : Math.floor(6/(this.state.attributes.length-1));
             data.push(
-                <Col xs={size} key={attribute}>
-                    {attribute.charAt(0).toUpperCase() + attribute.slice(1)}
+                <Col md={size} key={attribute}>
+                    {(attribute==='id') ? 'ID' : attribute.charAt(0).toUpperCase() + attribute.slice(1)}
                 </Col>
             );
         }
-        let size = !this.state.attributes.includes('name')
-            ? Math.floor(10/(this.state.attributes.length+2))
-            : Math.floor(6/(this.state.attributes.length+1));
-        data.push(
-            <React.Fragment key = 'header_distances'>
-                <Col xs={size} key="leg_distance">
-                    Leg Distance
-                </Col>
-                <Col xs={size} key="total_distance">
-                    Total Distance
-                </Col>
-            </React.Fragment>
-        );
         return data;
     }
 
@@ -154,13 +139,25 @@ class Itinerary extends Component {
                 <React.Fragment>
                     <Container>
                         <Row>
-                            {this.putHeader()}
+                            <Col xs={'10'}>
+                                <Row>
+                                    {this.putHeader()}
+                                </Row>
+                            </Col>
+                            <Col xs={'2'}/>
                         </Row>
                         <hr/>
                     </Container>
 
                     <Container style={style}>
                         {this.putData()}
+                    </Container>
+                    <Container>
+                        <Row key="Total">
+                            <Col sm="12" md={{size: 4, offset: 4}}>
+                                <b>Total Distance: <font color="green">{this.state.total}</font> {(this.props.trip.options.units==='user defined') ? this.props.trip.options.unitName : this.props.trip.options.units}</b>
+                            </Col>
+                        </Row>
                     </Container>
                 </React.Fragment>
             );
@@ -195,7 +192,7 @@ class Itinerary extends Component {
                     checked={this.state.attributes.includes(attribute)}
                     onChange={this.updateCheckbox}
                 />
-                {attribute.charAt(0).toUpperCase() + attribute.slice(1)}
+                {(attribute==='id') ? 'ID' : attribute.charAt(0).toUpperCase() + attribute.slice(1)}
                 &nbsp;&nbsp;
             </label>
         );
@@ -229,10 +226,11 @@ class Itinerary extends Component {
                                 </Button>
                             </Col>
                         </Row></CardTitle>
-                    {attributes}
-                    <br/>
-                    <hr/>
+
                     <Collapse isOpen={this.state.itin}>
+                        {attributes}
+                        <br/>
+                        <hr/>
                         {this.createTable()}
                     </Collapse>
                 </React.Fragment>
