e04d4655a8b9363adbe9f676e5004501cd557e0e
khanhnguyenle <kle@rams.colostate.edu>
2018-12-06 03:22:57 -0700

    front end test

--- client/test/Application.test.js
+++ client/test/Application.test.js
@@ -47,104 +47,19 @@ const startProps = {
     }
 };
 
-test('Check to see if real time debug modes are chosen correctly onclick', () => {
-    const options = mount((
-        <Application config={startProps.config} options={startProps.options}/>
-    ));
-
-    let actual = [];
-    options.find('Button').map((element) => actual.push(element.prop('value')));  // (2)
-    let realTime = actual.slice(0, 2);
-
-    expect(realTime).toEqual([true,false]);  // (3)
-});
-
-test('Check to see if units are chosen correctly onclick', () => {
-    const options = mount((
-        <Options config={startProps.config} options={startProps.options}/>
-    ));
-
-    let actual = [];
-    options.find('Button').map((element) => actual.push(element.prop('value')));  // (2)
-    let realTime = actual.slice(2, 6);
-
-    expect(realTime).toEqual(startProps.config.units);  // (3)
-});
-
-test('Check to see if maps are chosen correctly onclick', () => {
-    const options = mount((
-        <Options config={startProps.config} options={startProps.options}/>
-    ));
-
-    let actual = [];
-    options.find('Button').map((element) => actual.push(element.prop('value')));  // (2)
-    let poppedsubmitbuttons = actual.slice(7, 9);
-
-    expect(poppedsubmitbuttons).toEqual(["svg", "kml"]);  // (3)
-});
-
-test('Check to see if optimizations are chosen correctly onclick', () => {
-  const options = mount((
-      <Options config={startProps.config} options={startProps.options}/>
-    ));
-
-  let actual = [];
-  options.find('Button').map((element) => actual.push(element.prop('value')));  // (2)
-    let poppedsubmitbuttons = actual.slice(9, 13);
-
-  expect(poppedsubmitbuttons).toEqual(["none", "short", "shorter", "shortest"]);  // (3)
-});
-
-test('Test function updateUnits', () => {
-    const updateUnitsMock = jest.fn();
-    const updateOptionsMock = jest.fn();
-    const component = mount((
-        <Options config={startProps.config} options={startProps.options} updateUnits={updateUnitsMock} updateOptions={updateOptionsMock}/>
-    ));
-    component.find('#options_submit_units_field').at(0).simulate('click');
-});
-
-test('Test function userDefValues', () => {
-    const userDefValuesMock = jest.fn();
-    const updateOptionsMock = jest.fn();
-    const component = mount((
-        <Options config={startProps.config} options={startProps.options} userDefValues={userDefValuesMock} updateOptions={updateOptionsMock}/>
-    ));
-    component.find('#options_submit_userdefunits_field').at(0).simulate('click');
-    component.setState({ name: 'super miles' });
-    component.setState({ radius: 4000 });
-    component.find('#options_submit_userdefunits_field').at(0).simulate('click');
-});
-
-test('Test function updateHostAndPort', () => {
-    const updateHostAndPortMock = jest.fn();
-    const component = mount((
-        <Application config={startProps.config} options={startProps.options} updateHostAndPort={updateHostAndPortMock}/>
-    ));
-    component.find('#port_field').at(0).simulate('keyPress', { preventDefault(){}, alert(){}, key: '.' });
-    component.find('#port_field').at(0).simulate('keyPress', { preventDefault(){}, alert(){}, key: '+' });
-});
 
 test('Test function updateHostAndPort', () => {
     let o = new Application({});
     o.updateHostAndPort({host: 'localhost', port: 8080});
 });
 
+test('Test function updateOriginAndDestination', () => {
+    let o = new Application({});
+    o.updateOriginAndDestination({lat_f: 100, port: 280});
+});
+
 test('Test for small line of codes', () => {
-    const updateOptionsMock = jest.fn();
-    const updateHostAndPortMock = jest.fn();
-    const updateRealTimeMock = jest.fn();
-    const component = mount((
-        <Options config={startProps.config} options={startProps.options} updateOptions={updateOptionsMock} updateHostAndPort={updateHostAndPortMock} updateRealTime={updateRealTimeMock}/>
-    ));
-    component.find('#options_submit_opts_field').at(0).simulate('click');
-    component.find('#options_submit_maps_field').at(0).simulate('click');
-    component.find('#options_submit_rT_field').at(0).simulate('click');
-    component.find('#options_submit_hostport_field').at(0).simulate('click');
-    component.find('#host_field').at(0).simulate('change');
-    component.find('#port_field').at(0).simulate('change');
-    component.find('#unit_name_field').at(0).simulate('change');
-    component.find('#unit_radius_field').at(0).simulate('change');
+
 });
 
 
